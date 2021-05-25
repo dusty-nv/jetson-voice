21dac275412495b640cb99da2747d4520a194dd4
Dustin Franklin <dustinf@nvidia.com>
2021-05-25 11:25:29 -0400

    added config

--- jetson_voice/__init__.py
+++ jetson_voice/__init__.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
+
+from .config import config, ConfigDict, ConfigArgParser
+
+__version__ = config.version
---
+++ jetson_voice/config.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import json
+import pprint
+import logging
+import argparse
+
+
+#
+# Default global configuration
+#
+# This can be overriden at runtime with command-line options (see ConfigArgParser)
+# such as --global-config to load your own configuration from json file,
+# or by calling config.load('my_config.json')
+#
+# You can also set the options directly on the 'config' object, e.g.
+#
+#    config.model_dir = '/path/to/my/models'
+#    config.log_level = 'warning'
+#
+# It's recommended to use one of the methods above instead of changing _default_config directly.
+#
+_default_config = {
+    'version' : 0.1,
+    'model_dir' : 'data/networks',
+    'default_backend' : 'tensorrt',
+    'log_level' : 'info',
+    'debug' : False,
+    'profile' : False
+}
+
+
+class ConfigDict(dict):
+    """
+    Configuration dict that can be loaded from JSON and has members
+    accessible via attributes and can watch for updates to keys.
+    """
+    def __init__(self, *args, path=None, watch=None, **kwargs):
+        """
+        Parameters:
+          path (str) -- Path to JSON file to load from
+          
+          watch (function or dict) -- A callback function that gets called when a key is set.
+                                      Should a function signature like my_watch(key, value)
+                                      This can also be a dict of key names and functions,
+                                      and each function will only be called when it's particular
+                                      key has been set.  You can also subclass ConfigDict and
+                                      override the __watch__() member function.
+        """                                
+                                         
+        super(ConfigDict, self).__init__(*args, **kwargs)
+        
+        self.__dict__['path'] = path
+        self.__dict__['watch'] = watch
+        
+        for x in args:
+            if isinstance(x, dict):
+                for y in x:
+                    self.__watch__(y, x[y])
+                    
+        for x in kwargs:
+            self.__watch__(x, kwargs[x])
+               
+        if path:
+            self.load(path)
+            
+    def load(self, path, clear=False):
+        """
+        Load from JSON file.
+        """
+        if clear:
+            self.clear()
+            
+        with open(path) as file:
+            config_dict = json.load(file)
+        
+        self.update(config_dict)
+        
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        else:
+            return self[attr]
+        
+    def __setattr__(self, attr, value):
+        if attr in self.__dict__:
+            self.__dict__[attr] = value
+        else:
+            self[attr] = value
+        
+    def __setitem__(self, key, value):
+        if isinstance(value, dict):
+            value = ConfigDict(value, watch=self.watch)
+            
+        super(ConfigDict, self).__setitem__(key, value)
+        self.__watch__(key, value)
+    
+    def __watch__(self, key, value):
+        #print(f'watch {key} -> {value}')
+
+        if not self.watch:
+            return
+            
+        if isinstance(self.watch, dict):
+            if key in self.watch:
+                self.watch[key](key, value)
+        else:
+            self.watch(key, value)
+            
+    def __str__(self):
+        return pprint.pformat(self)
+        
+    #def __repr__(self):
+    #    return pprint.saferepr(self)
+        
+    def setdefault(self, key, default=None):
+        if isinstance(default, dict):
+            value = ConfigDict(value, watch=self.watch)
+        changed = key not in self
+        value = super(ConfigDict, self).setdefault(key, default)
+        if changed: self.__watch__(key, value)
+        
+    def update(self, *args, **kwargs):
+        for k, v in dict(*args, **kwargs).items():
+            self[k] = v
+        
+
+#
+# logging handlers
+#
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO) 
+
+config = None
+
+def _set_log_level(key, value):
+    log_value = value.upper()
+    
+    if log_value == 'VERBOSE':
+        log_value = 'DEBUG'
+        
+    log_level = getattr(logging, log_value, None)
+    
+    if not isinstance(log_level, int):
+        raise ValueError(f'Invalid log level: {value}')
+       
+    logging.getLogger().setLevel(log_level)
+    logging.debug(f'set logging level to {value}')
+
+    if config is not None and value.upper() == 'DEBUG':
+        config['debug'] = True
+    
+#
+# global config definition
+#
+config = ConfigDict(_default_config, watch={'log_level':_set_log_level})
+
+if config.log_level.upper() == 'DEBUG':
+    config['debug'] = True
+    
+logging.debug(f'global config:\n{config}')
+
+
+#
+# custom arg parser
+#
+class ConfigArgParser(argparse.ArgumentParser):
+    """
+    ArgumentParser that provides global configuration options.
+    """
+    def __init__(self, *args, **kwargs):
+        super(ConfigArgParser, self).__init__(*args, **kwargs)
+    
+        self.add_argument('--model-dir', default=_default_config['model_dir'], help=f"sets the root path of the models (default '{_default_config['model_dir']}')")
+        self.add_argument('--global-config', default=None, type=str, help='path to JSON file to load global configuration from')
+        self.add_argument('--profile', action='store_true', help='enables model performance profiling')
+        self.add_argument('--verbose', action='store_true', help='sets the logging level to verbose')
+        self.add_argument('--debug', action='store_true', help='sets the logging level to debug')
+        
+        log_levels = ['debug', 'verbose', 'info', 'warning', 'error', 'critical']
+        
+        self.add_argument('--log-level', default=_default_config['log_level'], type=str, choices=log_levels,
+                          help=f"sets the logging level to one of the options above (default={_default_config['log_level']})")
+        
+    def parse_args(self, *args, **kwargs):
+        args = super(ConfigArgParser, self).parse_args(*args, **kwargs)
+        
+        config.model_dir = args.model_dir
+        config.log_level = args.log_level
+        
+        if args.profile:
+            config.profile = True
+            
+        if args.verbose:
+            config.log_level = 'verbose'
+            
+        if args.debug:
+            config.log_level = 'debug'
+        
+        if args.global_config:
+            config.load(args.global_config)
+            
+        logging.debug(f'global config:\n{config}')    
+        return args
+

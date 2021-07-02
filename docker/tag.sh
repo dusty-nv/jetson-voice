6757a051189bea99d50db5d3474271ae64948f33
Dustin Franklin <dustinf@nvidia.com>
2021-07-02 10:47:17 -0400

    fixed container tag for L4T R32.5.1

--- docker/tag.sh
+++ docker/tag.sh
@@ -5,6 +5,10 @@ source scripts/os_version.sh
 
 if [ $ARCH = "aarch64" ]; then
 	TAG="r$L4T_VERSION"
+	
+	if [ $L4T_VERSION = "32.5.1" ]; then
+		TAG="r32.5.0"
+	fi	
 elif [ $ARCH = "x86_64" ]; then
 	TAG="$ARCH"
 else
